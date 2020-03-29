9f25c306946572a017655f820a24ed03e966ecbf
EswarSai106 <53952500+EswarSai106@users.noreply.github.com>
2020-03-29 17:37:10 +0530

    Update shortestjobfirst.c

--- shortestjobfirst.c
+++ shortestjobfirst.c
@@ -4,10 +4,13 @@
 struct process
 {
 int flag;
-int p_no,arrival_t,burst_t,ct,wait_t,taround_time,p;
+int p_no,arrival_t,burst_t,ct,wait_t,taround_time,p;  //p_no,arrival_t,burst_t,ct,wait_t,taround_t,p are process number,arrival time
+                               //burst time,completion time,waiting time,turn around time respectively.
  }p_list[250];
-int n;
- void Sort()
+int n; // n is for number of process
+ 
+
+void swap()   //swapping function based on arrival time
  {
 struct process p;
 
@@ -17,7 +20,7 @@ for(i=0;i<n-1;i++)
 {
 for(j=i+1;j<n;j++)
 {
-if(p_list[i].arrival_t > p_list[j].arrival_t)
+if(p_list[i].arrival_t > p_list[j].arrival_t)   //swaping
 {
 p = p_list[i];
 p_list[i] = p_list[j];
@@ -26,7 +29,7 @@ p_list[j] = p;
 }
 }
  }
- int main()
+ int main()  //main function
  {
 
 int i,t=0,b_t=0,peak;
@@ -38,7 +41,6 @@ float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
 printf("please enter number of processes: ");
 scanf("%d",&n);
 for(i = 0; i < n; i++)
-
 {
 
 p_list[i].p_no = i+1;
@@ -51,7 +53,7 @@ scanf("%d", &p_list[i].burst_t);
 p_list[i].flag = 0;
 b_t = b_t + p_list[i].burst_t;
 }
-Sort();
+swap();  //call for swap function
 for(i=0;i<n;i++)
 {
 a[i]=p_list[i].burst_t;
@@ -69,7 +71,7 @@ peak = i;
 if(p_list[peak].burst_t==0 && p_list[i].flag != 1)
 {
 p_list[i].flag = 1;
-p_list[peak].ct=t;p_list[peak].burst_t=8887;
+p_list[peak].ct=t;p_list[peak].burst_t=9999;
 printf("P%d  process completed in %d time\n ",p_list[i].p_no,p_list[peak].ct);
 }
 }
@@ -78,15 +80,15 @@ t++;
 }
 for(i=0;i<n;i++)
 {
-p_list[i].taround_time=(p_list[i].ct)-(p_list[i].arrival_t);
-avg_taround_time=avg_taround_time+p_list[i].taround_time;
-p_list[i].wait_t=((p_list[i].taround_time)-a[i]);
-avg_w_t=avg_w_t+p_list[i].wait_t;
+p_list[i].taround_time=(p_list[i].ct)-(p_list[i].arrival_t); //turn around time calculation
+avg_taround_time=avg_taround_time+p_list[i].taround_time; //average turn aaround time
+p_list[i].wait_t=((p_list[i].taround_time)-a[i]); // waiting time
+avg_w_t=avg_w_t+p_list[i].wait_t; // average waiting time
 }
 printf("PNO\tAT\tCT\tTA\tWTt\n");
 for(i=0;i<n;i++)
 {
-printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time
+printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time // printing the results
 ,p_list[i].wait_t);
  }
 printf("average turn around time is : %f\t\n\n",avg_taround_time);
